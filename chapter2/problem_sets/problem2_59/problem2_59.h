5d6078e13f3241968ea08f663b320e1298994b73
Andre A. Marin <andre@alexamarin.com>
2020-04-09 13:08:24 -0500

    Add unit test infrastructure for problem 2.59

---
+++ chapter2/problem_sets/problem2_59/problem2_59.h
@@ -0,0 +1,6 @@
+#ifndef PROBLEM2_59
+#define PROBLEM2_59
+
+int prob2_59(int x, int y);
+
+#endif
---
+++ tests/test_problem2_59.c
@@ -0,0 +1,11 @@
+#include "unity.h"
+#include "problem2_59.h"
+
+void setUp(void) {}
+void tearDown(void) {}
+
+int main(void)
+{
+UNITY_BEGIN();
+return UNITY_END();
+}
