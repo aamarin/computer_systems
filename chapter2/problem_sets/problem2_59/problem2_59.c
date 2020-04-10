d411a7cf10b4d2c095ef768dffd67fb0589968ef
Andre A. Marin <andre@alexamarin.com>
2020-04-09 19:59:06 -0500

    Update complete test cases for problem 2.59

--- chapter2/problem_sets/problem2_59/problem2_59.c
+++ chapter2/problem_sets/problem2_59/problem2_59.c
@@ -3,8 +3,6 @@
 
 int prob2_59(int x, int y) {
     int x_lower_byte = 0x0FF & x;
-    printf("0x%08x\n", x_lower_byte);
     int y_upper = (~0u << 8) & y;
-    printf("0x%08x\n", (~0u << 8));
     return (x_lower_byte | y_upper);
 }
--- tests/test_problem2_59.c
+++ tests/test_problem2_59.c
@@ -4,8 +4,23 @@
 void setUp(void) {}
 void tearDown(void) {}
 
-int main(void)
-{
-UNITY_BEGIN();
-return UNITY_END();
+void test_prob2_59_should_replaceLastByteWithHexEC(void) {
+    TEST_ASSERT_EQUAL_HEX32(0x123456EC, prob2_59(0xEC, 0x12345678));
+}
+
+void test_prob2_59_should_replaceLastByteWithHexFF(void) {
+    TEST_ASSERT_EQUAL_HEX32(0xFF, prob2_59(0xFF, 0x0));
+}
+
+void test_prob2_59_should_replaceLastByteWithHex00(void) {
+    TEST_ASSERT_EQUAL_HEX32(0x00, prob2_59(0x0, 0xFF));
+}
+
+int main(void) {
+    UNITY_BEGIN();
+    RUN_TEST(test_prob2_59_should_replaceLastByteWithHexEC);
+    RUN_TEST(test_prob2_59_should_replaceLastByteWithHexFF);
+    RUN_TEST(test_prob2_59_should_replaceLastByteWithHex00);
+
+    return UNITY_END();
 }
