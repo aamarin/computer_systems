67ad1f8889d488f5106f7af4ab361df1d6f482ea
Andre A. Marin <andre@alexamarin.com>
2020-04-11 15:32:56 -0500

    Simplify logic for problem 2.61

--- src/chapter2/src/problem2_61.c
+++ src/chapter2/src/problem2_61.c
@@ -8,18 +8,18 @@ int problem2_61(const int x) {
     // 3) Relative comparison operators
 
     // A) Any it of x equals 1 (can only fail for the all 0's case)
-    int is_any_bit_1 = x & (~0);
+    const int is_any_bit_1 = x & (~0);
 
     // B) Any bit of x equals 0 (can only fail for all 1's case)
-    int is_any_bit_0 = x ^ (~0);
+    const int is_any_bit_0 = x ^ (~0);
 
     // C) Any bit in the least significant byte (LSB) of x equal 1
     // Same as (A) but on the LSB.
-    int is_x_lsb_any_bit_1 = (x & 0xFF);
+    const int is_x_lsb_any_bit_1 = (is_any_bit_1 & 0xFF);
 
     // D) Any bit in the least significant byte (LSB) of x equal 0
     // Same as (B) but on the LSB.
-    int is_x_lsb_any_bit_0 = (x & 0xFF) ^ 0xFF;
+    const int is_x_lsb_any_bit_0 = is_any_bit_0 & 0xFF;
 
     return is_any_bit_1 && is_any_bit_0 && is_x_lsb_any_bit_1 && is_x_lsb_any_bit_0;  
 }
